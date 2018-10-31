eee89b0a053ac3e080c2e6abccbc99186807569e
Kirill Smirnov <smirnowku@gmail.com>
2018-10-31 20:54:31 +0300

    Implement apartments linear regression

--- linearregression/apartment.py
+++ linearregression/apartment.py
@@ -19,12 +19,12 @@ class ApartmentCsvReader(CsvReader):
 class Apartment:
 
     def __init__(self, area: float, rooms: int, price: float):
-        self.__area = float(area)
-        self.__rooms = int(rooms)
-        self.__price = float(price)
+        self.area = float(area)
+        self.rooms = int(rooms)
+        self.price = float(price)
 
     def __str__(self) -> str:
-        return "{area=%.1f,rooms=%i,price=%.1f}" % (self.__area, self.__rooms, self.__price)
+        return "{area=%.1f,rooms=%i,price=%.1f}" % (self.area, self.rooms, self.price)
 
     def __repr__(self):
         return str(self)
--- linearregression/main.py
+++ linearregression/main.py
@@ -1,4 +1,20 @@
+from sklearn.linear_model import LinearRegression
+
 from linearregression.apartment import *
 
 apartments = ApartmentCsvReader.read_apartments()
 print(apartments)
+
+X = [[a.area, a.rooms] for a in apartments]
+y = [a.price for a in apartments]
+print('X = %s\nY = %s' % (X, y))
+
+lr = LinearRegression(fit_intercept=True, normalize=True)
+lr.fit(X, y)
+print("Coeff = %s" % [lr.intercept_, lr.coef_])
+print("MSE = %e" % (sum((lr.predict(X) - y) ** 2) / len(X)))
+
+while True:
+    print("Input apartment: <area> <rooms>")
+    area, rooms = map(float, input().split())
+    print("Predicted price = %f" % lr.predict([[area, rooms]])[0])
