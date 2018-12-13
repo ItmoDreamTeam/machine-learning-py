db5f085874f82b8fa1b5a7be7046a82098659303
Kirill Smirnov <smirnowku@gmail.com>
2018-12-13 16:20:23 +0300

    Try to have fun on Wilcoxon

--- svm/wilcoxon.py
+++ svm/wilcoxon.py
@@ -15,8 +15,28 @@ svm = [
     0.828829,
     0.831858
 ]
+N = len(knn)
 
-print(numpy.subtract(knn, svm))
+difference = list(numpy.subtract(svm, knn))
+
+print(difference)
+difference.sort(key=lambda v: abs(v))
+
+rank = {}
+for i, diff in enumerate(difference):
+    rank[i] = diff
+
+print([r for r in rank if rank[r] > 0])
+
+Wn = abs(sum([r for r in rank if rank[r] < 0]))
+Wp = abs(sum([r for r in rank if rank[r] > 0]))
+W = min(Wp, Wn)
+
+total = sum([diff for i, diff in enumerate(difference) if rank[i] > 0])
+mu = N * (N + 1) / 4
+sigma = (N * (N + 1) * (2 * N + 1) / 24) ** 0.5
+z = (W - mu) / sigma
+print("z", z)
 
 p = wilcoxon(svm, knn)[1]
 print("p = %f" % p)
