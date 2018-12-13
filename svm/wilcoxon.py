97f9b0fc38675af6a1678a3613fc33bf29a36b2f
Kirill Smirnov <smirnowku@gmail.com>
2018-12-13 14:37:50 +0300

    Implement wilcoxon

--- svm/wilcoxon.py
+++ svm/wilcoxon.py
@@ -1,12 +1,22 @@
+import numpy
 from scipy.stats import wilcoxon
 
 knn = [
-    0
+    0.757895,
+    0.754098,
+    0.796460,
+    0.756757,
+    0.813559
 ]
 svm = [
-    1
+    0.857143,
+    0.833333,
+    0.785714,
+    0.828829,
+    0.831858
 ]
 
-T, p = wilcoxon(knn, svm)
-print("T = %f" % T)
+print(numpy.subtract(knn, svm))
+
+p = wilcoxon(svm, knn)[1]
 print("p = %f" % p)
