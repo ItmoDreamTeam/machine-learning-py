03fe546affa8a66fa5d036760a2dc8f282f2860a
Kirill Smirnov <smirnowku@gmail.com>
2018-12-18 18:53:13 +0300

    Implement Wilcoxon sum of ranks computation

--- svm/wilcoxon.py
+++ svm/wilcoxon.py
@@ -1,42 +1,56 @@
-import numpy
-from scipy.stats import wilcoxon
+def wilcoxon(a: [], b: []) -> {}:
+    """
+    Wilcoxon test \n
+    Null hypothesis: difference between the pairs follows a symmetric distribution around zero. \n
+    If the test statistic T <= critical_value, the null hypothesis is rejected. \n
+    :return: True if null hypothesis is true, False otherwise, T, critical_value
+    """
+    differences = sorted([b[i] - a[i] for i in range(len(a))], key=abs)
+    ranks = {i + 1: difference for i, difference in enumerate(differences)}
+    T = min(sum(filter(lambda rank: ranks[rank] > 0, ranks)), sum(filter(lambda rank: ranks[rank] < 0, ranks)))
+    cv = critical_value(len(a))
+    return {"Null hypothesis": T > cv, "test statistic": T, "critical value": cv}
+
+
+def critical_value(N: int) -> int:
+    """
+    α=0.05, two-tailed test \n
+    Two-tailed: most common, when we want to see if A > B or A < B or A = B \n
+    One-tailed: having a specific prediction (e.g., A is higher than B), we are completely uninterested
+    in the possibility that the opposite outcome could be true \n
+    :param N: number of data point, 6 &lt= N &lt= 20
+    :return:
+    """
+    return ([-1] * 6 + [1, 2, 4, 6, 8, 11, 14, 17, 21, 25, 30, 35, 40, 46, 52])[N]
+
 
 knn = [
     0.757895,
     0.754098,
     0.796460,
     0.756757,
-    0.813559
+    0.813559,
+    0.800000,
+    0.803738,
+    0.779661,
+    0.784000,
+    0.775862
 ]
 svm = [
     0.857143,
     0.833333,
     0.785714,
     0.828829,
-    0.831858
+    0.831858,
+    0.821429,
+    0.831858,
+    0.834783,
+    0.810811,
+    0.827586
 ]
-N = len(knn)
 
-difference = list(numpy.subtract(svm, knn))
+if __name__ == '__main__':
+    import scipy.stats
 
-print(difference)
-difference.sort(key=lambda v: abs(v))
-
-rank = {}
-for i, diff in enumerate(difference):
-    rank[i] = diff
-
-print([r for r in rank if rank[r] > 0])
-
-Wn = abs(sum([r for r in rank if rank[r] < 0]))
-Wp = abs(sum([r for r in rank if rank[r] > 0]))
-W = min(Wp, Wn)
-
-total = sum([diff for i, diff in enumerate(difference) if rank[i] > 0])
-mu = N * (N + 1) / 4
-sigma = (N * (N + 1) * (2 * N + 1) / 24) ** 0.5
-z = (W - mu) / sigma
-print("z", z)
-
-p = wilcoxon(svm, knn)[1]
-print("p = %f" % p)
+    print(wilcoxon(knn, svm))
+    print(scipy.stats.wilcoxon(knn, svm))
